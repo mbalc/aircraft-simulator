52231de03c6cb93840b49c31807be0ea91f99f49
mbalc <mbalc909@gmail.com>
2018-06-13 21:05:26 +0200

    Adapt backend crew tests to frontend

--- flights/tests.py
+++ flights/tests.py
@@ -1,21 +1,20 @@
 """Unit and Selenium test package for Flights app"""
 import json
-from datetime import datetime, timedelta
 
 from django.contrib.auth.models import User
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.db import IntegrityError
 from django.test import TestCase
 from django.utils import timezone
-
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 from .models import Plane, Crew, Flight, Airport, Reservation, Passenger
 
 
 def init_database():
     """Fill database with initial data"""
-    now = datetime.now(tz=timezone.utc)
+    now = timezone.now()
     airport1 = Airport.objects.create(name="a1")
     airport2 = Airport.objects.create(name="a2")
     plane1 = Plane.objects.create(identifier="p1", passengerLimit=20)
@@ -23,19 +22,18 @@ def init_database():
     # @formatter:off
     flight1 = Flight.objects \
         .create(plane=plane1,
-                takeoffAirport=airport1, takeoffTime=now - timedelta(hours=3, minutes=13),
-                landingAirport=airport2, landingTime=now + timedelta(hours=1),
+                takeoffAirport=airport1, takeoffTime=now.replace(hour=13, minute=13),
+                landingAirport=airport2, landingTime=now.replace(hour=16, minute=48),
                )
     flight2 = Flight.objects \
         .create(plane=plane2,
-                takeoffAirport=airport2, takeoffTime=now - timedelta(hours=3),
-                landingAirport=airport1, landingTime=now + timedelta(hours=1, minutes=25),
+                takeoffAirport=airport2, takeoffTime=now.replace(hour=15, minute=52),
+                landingAirport=airport1, landingTime=now.replace(hour=17, minute=12),
                )
     flight3 = Flight.objects \
         .create(plane=plane2,
-                takeoffAirport=airport1,
-                takeoffTime=now + timedelta(days=1, hours=1, minutes=41),
-                landingAirport=airport2, landingTime=now + timedelta(days=1, hours=5),
+                takeoffAirport=airport1, takeoffTime=now.replace(hour=18, minute=6),
+                landingAirport=airport2, landingTime=now.replace(hour=20, minute=17),
                )
     # @formatter:on
     crews = map(lambda num: Crew.objects.create(cptName='n%d' % num, cptSurname='s%d' % num),
@@ -46,8 +44,31 @@ def init_database():
         elem.save()
 
 
+def check_crew_simultaneous_flights(self):
+    """Don't allow a crew to fly two flights at the same time"""
+    self.check_binding_response(2, 0, True, 2)  # 2 . .
+    self.check_binding_response(2, 1, False, None)  # flights 0 and 1 at same time
+    self.check_binding_response(0, 2, True, 0)  # 2 . 0
+    self.check_binding_response(1, 1, True, 1)  # 2 1 0
+    self.check_binding_response(2, 1, False, 1)
+    self.check_binding_response(1, 0, False, 2)
+
+
+def check_crew_reassignment(self):
+    """Check if reassigning flight crews works as expected"""
+    self.check_binding_response(0, 0, True, 0)  # 0 . .
+    self.check_binding_response(1, 2, True, 1)  # 0 . 1
+    self.check_binding_response(1, 1, True, 1)  # 0 1 1
+    self.check_binding_response(0, 2, True, 0)  # 0 1 0
+    self.check_binding_response(2, 0, True, 2)  # 2 1 0
+    self.check_binding_response(0, 1, True, 0)  # 2 0 0
+
+
 class UITest(StaticLiveServerTestCase):
     """Test behaviour of frontend and effects of its usage on backend"""
+    fixtures = ['testdata.json']
+
+    creds = {'user': 'asdf', 'pass': 'qwer'}
 
     def get_user(self):
         """Return default username credential"""
@@ -70,15 +91,14 @@ class UITest(StaticLiveServerTestCase):
         """Change current page to the main one"""
         return self.driver.get('%s%s' % (self.live_server_url, '/'))
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.driver = webdriver.Firefox()
+
     def setUp(self):
         """Add initial data to db and authenticate future POST requests"""
-        self.driver = webdriver.Firefox()
-
-        init_database()
-
-        self.creds = {'user': 'asdf', 'pass': 'qwer'}
         User.objects.create_user(username=self.get_user(), password=self.get_pass())
-
         self.visit_home()
         self.login()
 
@@ -138,6 +158,33 @@ class UITest(StaticLiveServerTestCase):
         self.assertEqual("Your request is not right somehow",
                          driver.find_element_by_xpath("//h1").text)
 
+    def check_binding_response(self, crew_index, flight_index, should_succeed, expected_crew):
+        """Make a request and check if its results are relevant to what we expected"""
+        driver = self.driver
+        driver.implicitly_wait(3)
+
+        Select(driver.find_element_by_id("crewSelection")).select_by_value(str(crew_index + 1))
+        Select(driver.find_element_by_id("flightSelection")).select_by_value(str(flight_index + 1))
+        driver.find_element_by_xpath("(//button[@type='submit'])[2]").click()
+        if should_succeed:
+            self.assertTrue("success" in self.driver.find_element_by_id("request-status").text)
+        else:
+            self.assertTrue("issue" in self.driver.find_element_by_id("request-status").text)
+
+        if isinstance(expected_crew, int):
+            expected_crew = Crew.objects.all()[expected_crew]
+        self.assertEqual(Flight.objects.all()[flight_index].crew, expected_crew)
+
+    def test_crew_simultaneous_flights(self):
+        """Don't allow a crew to fly two flights at the same time - frontend"""
+        self.driver.find_element_by_link_text("Manage crews").click()
+        check_crew_simultaneous_flights(self)
+
+    def test_crew_reassignment(self):
+        """Check if reassigning flight crews works as expected - frontend"""
+        self.driver.find_element_by_link_text("Manage crews").click()
+        check_crew_reassignment(self)
+
 
 class CrewsTest(TestCase):
     """Unit tests for crew assignment code correctness"""
@@ -176,20 +223,10 @@ class CrewsTest(TestCase):
         self.assertRaises(IntegrityError, Crew.objects.create,
                           cptName=crew.cptName, cptSurname=crew.cptSurname)
 
-    def test_crew_two_flights_same_time(self):
-        """Don't allow a crew to fly two flights at the same time"""
-        self.check_binding_response(2, 0, True, 2)  # 2 . .
-        self.check_binding_response(2, 1, False, None)  # flights 0 and 1 at same time
-        self.check_binding_response(0, 2, True, 0)  # 2 . 0
-        self.check_binding_response(1, 1, True, 1)  # 2 1 0
-        self.check_binding_response(2, 1, False, 1)
-        self.check_binding_response(1, 0, False, 2)
+    def test_crew_simultaneous_flights(self):
+        """Don't allow a crew to fly two flights at the same time - backend"""
+        check_crew_simultaneous_flights(self)
 
     def test_crew_reassignment(self):
-        """Check if reassigning flight crews works as expected"""
-        self.check_binding_response(0, 0, True, 0)  # 0 . .
-        self.check_binding_response(1, 2, True, 1)  # 0 . 1
-        self.check_binding_response(1, 1, True, 1)  # 0 1 1
-        self.check_binding_response(0, 2, True, 0)  # 0 1 0
-        self.check_binding_response(2, 0, True, 2)  # 2 1 0
-        self.check_binding_response(0, 1, True, 0)  # 2 0 0
+        """Check if reassigning flight crews works as expected - backend"""
+        check_crew_reassignment(self)
